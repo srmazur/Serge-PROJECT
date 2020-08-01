e990234627a2bcee136b801b1ea51f9a23f964f8
serhii mazur <mazyr.sergey@gmail.com>
2020-08-01 14:53:00 -0400

    Simply ForLOop

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main{
 	public static void main(String[] args){
 	System.out.println("Hello World");
 	System.out.println("Second line");
+
+	int m = 0;
+	for (int i = 0 ;i <= 100 ; i ++){
+		System.out.println(m+=i);
+	}
 	}
 }
